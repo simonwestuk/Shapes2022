2e9d33e6fd6e86c17ea958e0530a7db1676d4783
wests <wests@C-010053>
2022-11-29 09:27:29 +0000

    Add project files.

---
+++ Shapes/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Shapes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape square = new Shape(4);
+            Shape triangle = new Shape(3);
+            Shape circle = new Shape(0);
+
+            Console.WriteLine(square.NoOfSides);
+            Console.WriteLine(triangle.NoOfSides);
+            Console.WriteLine(circle.NoOfSides);
+        }
+    }
+}
---
+++ Shapes/Shape.cs
@@ -0,0 +1,12 @@
+﻿namespace Shapes
+{
+    internal class Shape
+    {
+        public int NoOfSides;
+
+        public Shape(int _noOfSides)
+        {
+           NoOfSides = _noOfSides;
+        }
+    }
+}
